e844105fd9d06625c33f4471b3b585712062c619
dalamo20 <dalamo90@gmail.com>
2024-04-07 15:32:37 -0500

    readme updated

--- week4/server/index.js
+++ week4/server/index.js
@@ -8,6 +8,6 @@ app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/src"));
 
-app.listen(3000, function () {
+app.listen(4000, function () {
   console.log("Server started at http://localhost%s", port);
 });
